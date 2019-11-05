cc28c000f73581b6e12e47fd4d1d627af1a329f8
Tegan Broderick <teganbroderick@gmail.com>
2019-11-05 22:54:52 +0000

    Created map.html, added server routes for making a new map and adding map info to the maps table

--- server.py
+++ server.py
@@ -69,7 +69,7 @@ def signup_process():
     #check to see if email address is in the user table
     user_email = User.query.filter_by(email=email).first()
 
-    if user_email == None: #if user email is not in the user table
+    if user_email == None: #if user email is not in the users table
         #add user to database
         user_info = User(fname=fname, lname=lname, email=email, password=password)
         db.session.add(user_info)
@@ -87,7 +87,7 @@ def signup_process():
         return render_template("profile.html", fname=user.fname, lname=user.lname)
     
     else: 
-        flash("User with that email address already exists.")
+        flash("A user with that email address already exists.")
         return redirect("/login")
 
 @app.route("/logout")
@@ -102,6 +102,35 @@ def logout():
     print("")
     print("")
     return redirect('/')
+
+
+@app.route("/make_map")
+def makemap():
+    """redirect to new_map.html"""
+
+    return render_template("newmap.html")
+
+
+@app.route("/make_map_process")
+def makemap_process():
+
+    map_name = request.args.get("map_name")
+    map_description = request.args.get("map_description")
+
+    #check to see if map_name and map_description are already in the maps table for logged in user
+    map_to_verify = Map.query.filter_by(user_id=session['user_id'], map_name=map_name, map_description=map_description).first()
+    
+    if map_to_verify == None: #if new map is not in the maps table
+        #Add new map to maps table in db
+        map_info = Map(user_id=session['user_id'], map_name=map_name, map_description=map_description)
+        db.session.add(map_info)
+        db.session.commit()
+        #get map object
+        newmap = Map.query.filter_by(map_name=map_name, map_description=map_description).first()
+        return render_template("map.html", newmap=newmap)
+    else:
+        flash("A map with that name and description already exists.")
+        return redirect("/make_map")
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
