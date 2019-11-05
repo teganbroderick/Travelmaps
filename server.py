90018e40a39ddc199bb9593fd88a64858b1f0ab1
Tegan Broderick <teganbroderick@gmail.com>
2019-11-05 01:23:09 +0000

    Initial files

---
+++ server.py
@@ -0,0 +1,46 @@
+from jinja2 import StrictUndefined
+
+from flask import Flask, render_template, redirect, request, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import User, Map, Place, UserPlace, connect_to_db, db
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+# Normally, if you use an undefined variable in Jinja2, it fails
+# silently. This is horrible. Fix this so that, instead, it raises an
+# error.
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def index():
+    """Homepage"""
+
+    return render_template("homepage.html")
+
+
+
+@app.route("/login")
+def login():
+    """redirect to login.html"""
+
+    return render_template("loginin.html")
+
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+    # make sure templates, etc. are not cached in debug mode
+    app.jinja_env.auto_reload = app.debug
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(port=5000, host='0.0.0.0')
