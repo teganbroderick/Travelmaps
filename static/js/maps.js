55e74e9e5c13da4ab3656c69c46451a3f36abd3d
Tegan Broderick <teganbroderick@gmail.com>
2019-11-06 01:25:53 +0000

    Embedded test map into map.html

---
+++ static/js/maps.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// We use a function declaration for initMap because we actually *do* need
+// to rely on value-hoisting in this circumstance.
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+
+
+
