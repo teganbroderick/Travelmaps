887099a015073f7d83f48a7002c9ab5395d2d847
Tegan Broderick <teganbroderick@gmail.com>
2019-11-08 20:05:40 +0000

    Added bootstrap css features

--- server.py
+++ server.py
@@ -189,6 +189,6 @@ if __name__ == "__main__":
     connect_to_db(app)
 
     # Use the DebugToolbar
-    DebugToolbarExtension(app)
+    #DebugToolbarExtension(app)
 
     app.run(port=5000, host='0.0.0.0')
