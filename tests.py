c8d3d127c5ccbf4524b19130a080796f940e3eb7
Tegan Broderick <teganbroderick@gmail.com>
2019-11-22 22:32:03 +0000

    Created tests for /login and /about pages loading sucessfully

--- tests.py
+++ tests.py
@@ -9,11 +9,21 @@ class FlaskTests(TestCase):
         self.client = app.test_client()
         app.config['TESTING'] = True
 
-    def test_flask_route(self):
+
+    def test_index_route(self):
         result = self.client.get("/")
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'<div class="bg"></div>', result.data)
 
+    def test_login_route(self):
+        result = self.client.get("/login")
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'<h2>Login</h2>', result.data)        
+
+    def test_about_route(self):
+        result = self.client.get("/about")
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'<h2>About Travel Maps</h2>', result.data) 
 
 if __name__ == '__main__':
     import unittest
