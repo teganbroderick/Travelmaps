7e50c7bd85e7bc7431d70afc943403fc075db6bf
Tegan Broderick <teganbroderick@gmail.com>
2019-11-23 00:27:49 +0000

    Added example_data() to model.py. Added test_login_process() to tests.py

--- tests.py
+++ tests.py
@@ -1,13 +1,23 @@
 from server import app
 from unittest import TestCase
+from model import connect_to_db, db, example_data
+from flask import session
 
 class FlaskTests(TestCase):
     """Test flask routes"""
 
     def setUp(self):
         """Things to do before every test"""
+        
+        #get flask test client
         self.client = app.test_client()
         app.config['TESTING'] = True
+
+        #connect to test database
+        connect_to_db(app, "postgresql:///testdb")
+
+        db.create_all()
+        example_data()
 
 
     def test_index_route(self):
@@ -24,6 +34,12 @@ class FlaskTests(TestCase):
         result = self.client.get("/about")
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'<h2>About Travel Maps</h2>', result.data) 
+
+    def test_login_process(self):
+        result = self.client.post("/login_process", 
+            data={"email":"kknight@example.com", "password":"pinnacle"},
+            follow_redirects="True")
+        self.assertIn(b"Hello, Karl Knight !", result.data)
 
 if __name__ == '__main__':
     import unittest
