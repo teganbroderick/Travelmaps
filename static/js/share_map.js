5ddf26e94e1febb992d1695e1a8ba0b77fb59e57
Tegan Broderick <teganbroderick@gmail.com>
2019-12-05 20:33:08 +0000

    Cleaned up code and comments

--- static/js/share_map.js
+++ static/js/share_map.js
@@ -2,15 +2,15 @@
 
 function initAutocomplete() {
 
-  //INSTANTIATE MAP
+  // INSTANTIATE MAP
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     mapTypeId: 'roadmap'
   });
 
   // CENTER MAP
-  //find coordinates of last active place added, center map on those coordinates
-  //If there are no places added yet, center map on SF
+  // Find coordinates of last active place added, center map on those coordinates
+  // If there are no places added yet, center map on SF
   function centerMap(response) {
       var centerCoords = {lat: response.latitude, lng: response.longitude};
       map.setCenter(centerCoords)
@@ -18,11 +18,11 @@ function initAutocomplete() {
  
   $.get('/get_last_place_added/', {map_id : map_id}, centerMap);
   
-  //LOAD AND RENDER SAVED MARKERS
-  //Array to save usermarkers
+  // LOAD AND RENDER SAVED MARKERS
+  // Array to save usermarkers
   var userMarkers = [];
 
-  //ajax call to get place information from server.py db, add saved markers to map
+  // Ajax call to get place information from server.py db, make markers, add markers to map
   $.get('/get_places/', {map_id : map_id}, makeMarkers);
 
   function makeMarkers(response) {
@@ -47,9 +47,8 @@ function initAutocomplete() {
       }));
     }
 
-    //make info windows for saved markers
-    //nb. need to add more columns to the db for address, types, website, opening hours, etc. 
-        for (const marker of userMarkers) {
+    // Define info windows for saved markers
+    for (const marker of userMarkers) {
       const markerInfo = (`
         <div id="infowindow-content">
           <p id="marker_heading" class="title">${marker.title}</p>
@@ -66,38 +65,34 @@ function initAutocomplete() {
         width: 200
       });
       
-      //event listener - click on marker, open infowindow
+      // Event listener - click on marker, open infowindow
       marker.addListener('click', () => {
         infoWindow.open(map, marker);
       });
     }
 
-  //EVENT LISTENER for clicking on a place name in the list, opening corresponding marker info window
+  // CLICK ON PLACE NAME IN LIST - CENTER MAP ON MARKER, OPEN INFO WINDOW
+  // Event listener
   $('a.place-name').on('click', function(evt) {
     evt.preventDefault();
     
-    //print test strings
-    console.log("User makers:", userMarkers);
-    console.log("You are in the place name area");
-
-    // get link data-name
+    // Get name from text in link
     var placeToFind = $(this).data('name');
-    console.log("Place to find: ", placeToFind);
 
-    // find map marker in markers array with the same title
+    // Find map marker in markers array with the same title
     var markerToClick;
     var markerCenterCoords;
     for (var i in userMarkers) {
       if(userMarkers[i].title === placeToFind) {
-        console.log(userMarkers[i]);
+        // console.log(userMarkers[i]);
         markerToClick = userMarkers[i];
         markerCenterCoords = {lat: userMarkers[i].latitude, lng: userMarkers[i].longitude};
       }
     }
-    console.log(markerCenterCoords)
-    //center map on coords of markerToClick
+    // console.log(markerCenterCoords)
+    // // Center map on coords of markerToClick
     map.setCenter(markerCenterCoords)
-    //trigger click event on the markerToClick
+    // Trigger click event on the markerToClick
     new google.maps.event.trigger(markerToClick, 'click');
   });
 
